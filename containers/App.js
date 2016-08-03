6136988457883c5a98dd950288cc92b9170831a4
skesepara <skesepara@ziraatteknoloji.com>
2016-08-03 10:07:55 +0300

    control is added

--- containers/App.js
+++ containers/App.js
@@ -6,11 +6,16 @@ import * as Actions from '../actions'
 import { Tabs, Tab } from 'react-bootstrap';
 
 class App extends Component {
+  handleSelect(key) {
+    console.log(key);
+    this.setState({key});
+  }
   render() {
     const { list, actions } = this.props
+    
     return (
       <div>
-        <Tabs defaultActiveKey={1} animation={false} id="noanim-tab-example">
+        <Tabs defaultActiveKey={1} onSelect={this.handleSelect} id="noanim-tab-example">
           <Tab eventKey={1} title="Tab 1"><Header add={actions.add} /></Tab>
           <Tab eventKey={2} title="Tab 2">Tab 2 content</Tab>
           <Tab eventKey={3} title="Tab 3">Tab 3 content</Tab>
